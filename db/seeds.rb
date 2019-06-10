cb63a902cd3e4dce6baba2c4a8fff7873706d5a3
Daniela Sandoval <daniela.sandoval820@gmail.com>
2019-06-09 21:23:36 -0400

    created url helper to toggle boolean

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Student.create(first_name: "Daniela", last_name: "Sandoval")
+Student.create(first_name: "Mochi", last_name: "Sandoval")
+Student.create(first_name: "Lychee", last_name: "Sandoval")
