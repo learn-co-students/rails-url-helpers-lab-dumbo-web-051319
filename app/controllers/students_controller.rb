b4421bb8fa191e737ea861e188944c9659d21898
Tess Neau <tessneau@gmail.com>
2019-06-10 10:20:01 -0400

    done

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,11 +1,19 @@
 class StudentsController < ApplicationController
   before_action :set_student, only: :show
-  
+
   def index
     @students = Student.all
   end
 
   def show
+    @student = set_student
+  end
+
+  def edit
+    @student = set_student
+    @student.switch_activity
+    @student.save
+    redirect_to student_path(@student)
   end
 
   private
--- db/seeds.rb
+++ db/seeds.rb
@@ -2,6 +2,6 @@
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
 # Examples:
-#
+Student.create([{first_name: 'Tess', last_name: 'Neau'}, {first_name: 'Grace', last_name: 'Gerber'}, {first_name: 'Tiny', last_name: 'Dancer'}, {first_name: 'Neve', last_name: 'Vene'}])
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
